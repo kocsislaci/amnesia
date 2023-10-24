bd35a39d1114bc091b2ed9f8fb60d6c7ed9945d4
Imre Racz <imre@riltech.co>
2023-10-24 13:30:04 +0200

    feat(camera): now fixed onto 'player' and makes walls transparent

--- Assets/Src/GameManager.cs
+++ Assets/Src/GameManager.cs
@@ -2,19 +2,8 @@ using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    private GameObject _placeholderPrefab;
-
     private void Start() {
-        Debug.Log("I started!");
-        StartCoroutine(StartDrawing());
-    }
-
-    private IEnumerator StartDrawing() {
-        yield return new WaitForSeconds(2);
-        Debug.Log("I waited for 2 seconds!");
-        _placeholderPrefab = Resources.Load<GameObject>("Level/Prefabs/Cube");
-        var placeHolderCube = Instantiate(_placeholderPrefab, new UnityEngine.Vector3(0, 2, 0), UnityEngine.Quaternion.identity);
-        yield return null;
+        Debug.Log("I started really thhough!");
     }
 }
 
