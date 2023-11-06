77eede7941c2ec732ba0a57e080eafcb8f77d137
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-11-06 12:33:36 +0100

    feat(layer): refactored player input system

---
+++ Assets/Src/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour {
+  private NavMeshAgent agent;
+  private PlayerControls playerControls;
+
+  void Awake() {
+    agent = GetComponent<NavMeshAgent>();
+    playerControls = new PlayerControls();
+    playerControls.Gameplay.LeftClick.performed += Move;
+  }
+  private void OnEnable() {
+    playerControls.Gameplay.Enable();
+  }
+
+  private void OnDisable() {
+    playerControls.Gameplay.Disable();
+  }
+
+  
+
+  private void Move(InputAction.CallbackContext context) {
+    Debug.Log("hit");
+    RaycastHit hit;
+
+    if (Physics.Raycast(Camera.main.ScreenPointToRay(playerControls.Gameplay.MousePosition.ReadValue<Vector2>()), out hit, 100))
+    {
+      agent.destination = hit.point;
+    }
+  }
+}
